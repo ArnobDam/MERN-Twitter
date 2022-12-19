7e91bef5e68deefaec88edfbca8d76af6e27c565
Arnob <arnobdam@gmail.com>
2022-12-18 20:49:12 -0500

    finished express error handling

--- backend/app.js
+++ backend/app.js
@@ -12,6 +12,8 @@ const csrfRouter = require('./routes/api/csrf');
 
 const app = express();
 
+const debug = require('debug')
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -19,23 +21,44 @@ app.use(cookieParser());
 
 // Security Middleware
 if (!isProduction) {
-  // Enable CORS only in development because React will be on the React
-  // development server (http://localhost:3000). (In production, React files
-  // will be served statically on the Express server.)
-  app.use(cors());
+    // Enable CORS only in development because React will be on the React
+    // development server (http://localhost:3000). (In production, React files
+    // will be served statically on the Express server.)
+    app.use(cors());
 }
 
 // Set the _csrf token and create req.csrfToken method to generate a hashed
 // CSRF token
 app.use(
-  csurf({
-    cookie: {
-      secure: isProduction,
-      sameSite: isProduction && "Lax",
-      httpOnly: true
-    }
-  })
+    csurf({
+        cookie: {
+            secure: isProduction,
+            sameSite: isProduction && "Lax",
+            httpOnly: true
+        }
+    })
 );
+
+app.use((req, res, next) => {
+    const err = new Error('Not Found');
+    err.statusCode = 404;
+    next(err);
+});
+
+const serverErrorLogger = debug('backend:error');
+
+// Express custom error handler that will be called whenever a route handler or
+// middleware throws an error or invokes the `next` function with a truthy value
+app.use((err, req, res, next) => {
+    serverErrorLogger(err);
+    const statusCode = err.statusCode || 500;
+    res.status(statusCode);
+    res.json({
+        message: err.message,
+        statusCode,
+        errors: err.errors
+    })
+});
 
 // Attach Express routers
 app.use('/api/users', usersRouter);
